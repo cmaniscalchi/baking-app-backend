9cf10e03704936e0aa82a5737eda6b51cf118944
cmaniscalchi <claudia.maniscalchi@gmail.com>
2018-11-27 13:22:07 -0500

    can save a recipe w/ nested attrubutes for multiple instances of recipe_ingredients, all user & ingredient associations are in place

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -5,9 +5,17 @@ class Api::V1::RecipesController < ApplicationController
     render json: @recipes
   end
 
+  def create
+    byebug
+    @recipe = Recipe.create(recipe_params)
+    render json: @recipe
+  end
+
   private
 
   def recipe_params
-    params.require(:recipe).permit(:recipe_name, :url, :multiplier, :conversion_unit, :user_id)
+    params.require(:recipe).permit(
+      :recipe_name, :url, :multiplier, :conversion_unit, :user_id,
+      recipe_ingredients_attributes: [ :recipe_ingredient_volume, :recipe_ingredient_unit, :recipe_ingredient_ounces, :recipe_ingredient_grams, :recipe_id, :ingredient_id ])
   end
 end
